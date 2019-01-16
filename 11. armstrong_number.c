a82a3939c5815b558a974669b3abff06ff6c7cfa
AnmolTomer <anmol3540@gmail.com>
2019-01-16 22:30:11 +0530

    Program to print Armstrong Number

---
+++ 11. armstrong_number.c
@@ -0,0 +1,20 @@
+// Any number is said to be an armstrong number if the sum of cube of every digit in the number is equal to the number itself.
+
+#include<stdio.h>
+
+main(){
+    int num,temp,sum,rem;
+    printf("Enter any number : ");
+    scanf("%d", &num);
+    temp = num; //Temporary number being used for checking if number is a armstrong number after further processing.
+    sum = 0;
+    while (temp>0){
+        rem = temp%10; //Remainder or mod operator to get the last digit and add its cube in the next step to sum.
+        sum += (rem*rem*rem);
+        temp /= 10; // To remove the last digit from the number we divide /.
+    }
+    if (num==sum)
+    printf("%d is an Armstrong Number.",sum);
+    else
+    printf("%d is not an Armstrong Number.",num);
+}
