cb637ae5ea53c016dc7c6e778e94f2cc5070c264
AnmolTomer <anmol3540@gmail.com>
2019-01-17 18:00:37 +0530

    Program to check if the entered character is a vowel or not.

---
+++ 13. vowel_or_not.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+
+	char ch;
+	printf("Enter any character : ");
+	scanf("%c",&ch);
+	switch(ch)
+	{
+		case 'A':
+			printf("%c is a vowel.",ch);
+			break;
+		case 'E':
+			printf("%c is a vowel.",ch);
+			break;
+		case 'I':
+			printf("%c is a vowel.",ch);
+			break;
+		case 'O':
+			printf("%c is a vowel.",ch);
+			break;
+		case 'U':
+			printf("%c is a vowel.",ch);
+			break;
+		default:
+			printf("%c is not a vowel.",ch);
+		
+	}
+}
